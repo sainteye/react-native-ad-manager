00a740d00c4dd39f8cf69f61c21e47fb5058a065
Sean Cheng <sainteye.x.deatheye@gmail.com>
2022-01-03 23:38:29 +0800

    export file

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
-export { default as Interstitial } from './src/CTKAdManagerInterstitial';
+export { default as Interstitial } from './src/CTKAdManagerInterstitial'
 
-export { default as Banner } from './src/CTKAdManagerBanner';
-export { default as NativeAdsManager } from './src/native-ads/NativeAdsManager';
-export {
-  default as TriggerableView,
-} from './src/native-ads/TriggerableViewManager';
-export { default as withNativeAd } from './src/native-ads/withNativeAd';
+export { default as Banner } from './src/CTKAdManagerBanner'
+export { default as NativeAdsManager } from './src/native-ads/NativeAdsManager'
+export { default as MediaView } from './src/native-ads/MediaViewManager'
+export { default as AdView } from './src/native-ads/AdViewManager'
+export { default as TriggerableView } from './src/native-ads/TriggerableViewManager'
+export { default as withNativeAd } from './src/native-ads/withNativeAd'
